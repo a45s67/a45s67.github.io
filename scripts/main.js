c01b08c278eb24dead94125f9662dfb69de5e3fc
fish Yu <b45s67@gmail.com>
2020-04-13 23:03:23 +0800

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,20 @@
 let myhead = document.querySelector('h1');
 myhead.textcontent = "Hi!!><";
-alert("Say Hi !!");
+// alert("Say Hi !!");
+
+var myimg = document.querySelector('img');
+myimg.onclick = function () {
+	let path = myimg.getAttribute('src');
+	if(path==="images/firefox-icon.png"){
+		myimg.setAttribute("images/123.PNG");
+	}
+	else{
+		myimg.setAttribute("images/firefox-icon.png");
+	}
+	
+// body...
+}
+
 
 function mult(a,b){
 	let c = a*b;
