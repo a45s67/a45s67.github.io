cd21cf5d01eab758068d02719e0dc2ca46e5d4a2
fish Yu <b45s67@gmail.com>
2020-04-13 22:49:02 +0800

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,9 @@
-var head = document.querySelector('h1');
-head.textContent = "Hi!!";
-alert("Say Hi!!");
+let myhead = document.querySelector('h1');
+myhead.textcontent = "Hi!!><";
+alert("Say Hi !!");
+
+function mult(a,b){
+	let c = a*b;
+	return c;
+}
+document.querySelector('html').onclick = function(){ alert("Don't poke me...")};
